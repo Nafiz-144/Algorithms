3b9d8f5a93ad28d562b074d34f0a5d9b0df66e1c
Nafiz144 <nafizahamed0464@gmail.com>
2024-09-21 23:26:06 +0600

    21-09-24

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -37,6 +37,58 @@ int main()
     else
     {
         cout << "Not found" << endl;
+    }
+
+    return 0;
+}*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search(vector<int> &a, int element)
+{
+    int low = 0, high = a.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (a[mid] == element)
+        {
+            return mid;
+        }
+        else if (element > a[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1; // Element not found
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int element;
+    cin >> element;
+
+    int result = binary_search(a, element);
+
+    if (result != -1)
+    {
+        cout << "Element found at index: " << result << endl;
+    }
+    else
+    {
+        cout << "Element not found" << endl;
     }
 
     return 0;
