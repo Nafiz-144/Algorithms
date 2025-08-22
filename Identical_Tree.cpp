c29b889e30a6c6e50033b168b7939a577e6cb31b
Nafiz144 <nafizahamed0464@gmail.com>
2025-08-22 20:53:06 +0600

    22

--- Identical_Tree.cpp
+++ Identical_Tree.cpp
@@ -1,7 +1,30 @@
 // same tree
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+
+// Definition for a binary tree node.
+struct TreeNode
 {
-    return 0;
-}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+                                                       right(right) {}
+};
+
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL || q == NULL)
+        {
+            return p == q;
+        }
+        bool ileft = isSameTree(p->left, q->left);
+        bool iright = isSameTree(p->right, q->right);
+        return ileft && iright && p->val == q->val;
+    }
+};
